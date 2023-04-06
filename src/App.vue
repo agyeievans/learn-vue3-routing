205454c456e88e96d8060a2c0a4ad7523cd52bf5
agyeievans <evanskofiagyei06@gmail.com>
2023-04-06 09:35:25 +0000

    added Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import {RouterView} from "vue-router"
-
+import Nav from "./components/Nav.vue"
 
 </script>
 
 <template>
   <div>
+    <Nav />
     <RouterView />
   </div>
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {RouterLink} from "vue-router"
+</script>
+
+<template>
+    <div>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+    .active{
+        font-weight: bolder;
+        color: rgb(153, 104, 14);
+        font-size: 20px;
+    }
+</style>
