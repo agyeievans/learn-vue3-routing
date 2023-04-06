08142bfcb5832b1968a7506e4d43646fc9568a91
agyeievans <evanskofiagyei06@gmail.com>
2023-04-06 11:53:24 +0000

    added redirect to home view

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 import CarView from "../views/CarView.vue"
 import ContactView from "../views/ContactView.vue";
+import NotFoundView from "../views/404View.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,10 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView
+        },
+        {
+            path: "/home",
+            redirect: "/"
         },
         {
             path: "/about",
@@ -30,6 +35,13 @@ const router = createRouter({
                     component: ContactView
                 }
             ]
+        },
+        {
+            //404 page 
+            path: "/:catchall(.*)*",
+            name: "notfound",
+            component: NotFoundView
+
         }
     ]
 })
---
+++ src/views/404View.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+            404 Page Not Found
+        </h1>
+    </div>
+</template>
